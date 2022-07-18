3c2378cad216999325a9b7f63089882641366fa5
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-18 07:55:54 -0300

    adv gen app

--- src/Advice.tsx
+++ src/Advice.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import iconDice from "./assets/images/icon-dice.svg";
 
 export const Advice = () => {
   return (
-    <div>
-      <h1>Advice</h1>
-    </div>
-  )
-}
+    <section className="container">
+      <div className="container__adivice">
+        <h1>Advice</h1>
+        <p>
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action"
+        </p>
+        <div className="container__btn">
+          <button className="btn"></button>
+        </div>
+      </div>
+    </section>
+  );
+};
